505d638bd6bda25934e6c9a54fcb1acd4535580a
husteve07 <husteve7@gmail.com>
2026-05-19 15:12:17 -0400

    refactor

--- main.go
+++ main.go
@@ -3,29 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
-	"fmt"
 	"sync/atomic"
 )
 
 type apiConfig struct {
-	fileServerHits atomic.Int32
+	fileserverHits atomic.Int32
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	cfg := &apiConfig{
-		fileServerHits: atomic.Int32{},
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
 	}
 
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir(filepathRoot))
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
 
-	mux.Handle("GET /app/", cfg.middlewareMetricInc(fileServer))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("GET /admin/metrics", cfg.handlerServerMetric)
-	mux.HandleFunc("POST /admin/reset", cfg.handlerResetMetric)
+
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -34,31 +34,4 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-
-func (cfg *apiConfig) handlerServerMetric(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileServerHits.Load())))
-}
-
-func (cfg *apiConfig) handlerResetMetric(w http.ResponseWriter, r *http.Request) {
-	cfg.fileServerHits.Store(0)
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
-}
-
-func (cfg *apiConfig) middlewareMetricInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileServerHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
 }
